5e6fe7bec1e31841360ca3d9b0e984eab956818d
Gilbert Parreno <gilbert.parreno@paysera.net>
2024-08-08 12:28:51 +0800

    update lifecyclecontroller

--- conductor-modules/arch-components-lifecycle/src/main/java/com/bluelinelabs/conductor/archlifecycle/LifecycleController.java
+++ conductor-modules/arch-components-lifecycle/src/main/java/com/bluelinelabs/conductor/archlifecycle/LifecycleController.java
@@ -5,11 +5,14 @@ import androidx.lifecycle.LifecycleOwner;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStore;
 
 import com.bluelinelabs.conductor.Controller;
 
 public abstract class LifecycleController extends Controller implements LifecycleOwner {
 
+    private final ViewModelStore viewModelStore = new ViewModelStore();
     private final ControllerLifecycleOwner lifecycleOwner = new ControllerLifecycleOwner(this);
 
     public LifecycleController() {
@@ -20,9 +23,30 @@ public abstract class LifecycleController extends Controller implements Lifecycl
         super(args);
     }
 
+    public ViewModelProvider viewModelProvider() {
+        return viewModelProvider(new ViewModelProvider.AndroidViewModelFactory(getActivity().getApplication()));
+    }
+
+    public ViewModelProvider viewModelProvider(ViewModelProvider.NewInstanceFactory factory) {
+        return new ViewModelProvider(viewModelStore, factory);
+    }
+
+    public ViewModelProvider viewModelProvider(ViewModelProvider.Factory factory) {
+        return new ViewModelProvider(viewModelStore, factory);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        viewModelStore.clear();
+    }
+
     @Override @NonNull
     public Lifecycle getLifecycle() {
         return lifecycleOwner.getLifecycle();
     }
 
+    public ControllerLifecycleOwner getLifecycleOwner() {
+        return lifecycleOwner;
+    }
 }
